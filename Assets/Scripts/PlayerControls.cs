f11f42d1cc2e288831b882fd341d68870d357166
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-09-03 10:43:29 +0300

    Argon Assault Commit
    ArgonAssault first commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+
+    [SerializeField] float positionYawFactor = 2.25f;
+    [SerializeField] float controlYawFactor = 0f;
+
+    [SerializeField] float positionRollFactor = 0f;
+    [SerializeField] float controlRollFactor = -25f;
+
+    [SerializeField] GameObject[] lasers;
+
+    float xThrow, yThrow;
+
+    private void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+        ProcessShooting();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+        
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYpos = Mathf.Clamp(rawYPos, -yRange, yRange);        
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
+    
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch =  pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yawDueToControlThrow = xThrow * controlYawFactor;
+        float yaw =  yawDueToPosition + yawDueToControlThrow;
+
+        float rollDueToPosition = transform.localPosition.x * positionRollFactor;
+        float rollDueToControlThrow = xThrow * controlRollFactor;
+        float roll =  rollDueToPosition + rollDueToControlThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessShooting()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            SetLasersActive(true);
+        }
+        else
+        {
+            SetLasersActive(false);
+        }
+    }
+
+    private void SetLasersActive(bool isActive)
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
+    }
+
+}
